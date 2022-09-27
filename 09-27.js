b5923608de02b16ac3fd86a2a905f3f59bf8e17c
joshyeom <105287510+joshyeom@users.noreply.github.com>
2022-09-27 17:21:06 +0900

    09/27__JS

---
+++ 09-27.js
@@ -0,0 +1,23 @@
+const no_1 = 1;
+const no_2 = 2;
+const no_3 = 3;
+
+function add() {
+  console.log(no_1 + no_2 + no_3);
+  alert(no_1 + no_2 + no_3);
+}
+
+function sub() {
+  console.log(no_1 - no_2);
+  alert(no_1 - no_2);
+}
+
+function divide() {
+  console.log(no_1 % no_2);
+  alert(no_1 % no_2);
+}
+
+function mul() {
+  console.log(no_1 * no_2);
+  alert(no_1 * no_2);
+}
